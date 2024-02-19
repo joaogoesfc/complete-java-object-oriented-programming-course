338e946429f57a84b922eb3be1763fbf5d1c2ece
João Góes <139176161+joaogoesfc@users.noreply.github.com>
2024-02-19 19:28:00 -0300

    Challenge 3: sum two numbers

---
+++ challenges/challenge003-sum-two-numbers/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		int sum = num1 + num2;
+		
+		System.out.println("Sum = " + sum);
+				
+		
+		sc.close();
+	}
+
+}
