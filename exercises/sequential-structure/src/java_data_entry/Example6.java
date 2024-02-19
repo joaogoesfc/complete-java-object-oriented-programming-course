217d330bdedcc99035931bf879ca64d53d439916
João Góes <139176161+joaogoesfc@users.noreply.github.com>
2024-02-19 17:30:34 -0300

    Exercise 1 of mathematical functions

--- exercises/sequential-structure/src/java_data_entry/Example6.java
+++ exercises/sequential-structure/src/java_data_entry/Example6.java
@@ -12,7 +12,7 @@ public class Example6 {
 		String s1, s2, s3;
 		
 		x = sc.nextInt();
-		sc.nextLine();
+		sc.nextLine(); // before a nextLine() use other without a variable to absorb the line break(enter)
 		s1 = sc.nextLine();
 		s2 = sc.nextLine();
 		s3 = sc.nextLine();
---
+++ exercises/sequential-structure/src/section_3_mathematical_functions/Exercise1.java
@@ -0,0 +1,33 @@
+package section_3_mathematical_functions;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.4;
+		double A, B, C;
+		
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		System.out.println("Square root of " + x + " = " + A);
+		System.out.println("Square root of " + y + " = " + B);
+		System.out.println("Square root of 25 = " + C);
+		
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println(x + "to the power of " + y + " = " + A);
+		System.out.println(x + "squared = " + B);
+		System.out.println("5 squared = " + C);
+		
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("Absolute value of " + y + " = " + A);
+		System.out.println("Absolute value of " + z + " = " + B);
+
+	}
+
+}
