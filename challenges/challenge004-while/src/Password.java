f0bc7755bf06a1d756163b125a395ca818cbafc7
João Góes <139176161+joaogoesfc@users.noreply.github.com>
2024-02-20 18:57:34 -0300

    Challenge 004-1 of while

---
+++ challenges/challenge004-while/src/Password.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Password {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int password = 2002;
+		System.out.print("Enter your password:");
+		int x = sc.nextInt();
+		while (x != password) {
+			System.out.println("Invalid password! try again");
+			x = sc.nextInt();
+		}
+		
+		System.out.println("Access allowed");
+		
+
+	}
+
+}
