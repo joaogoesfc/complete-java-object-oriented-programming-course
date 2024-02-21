a5dbfb76a984d2679f4ae85151b30d10cb3a80f6
João Góes <139176161+joaogoesfc@users.noreply.github.com>
2024-02-21 18:11:19 -0300

    Challenge005 - C3 of loop for

---
+++ challenges/challenge005-for/src/C3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class C3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		System.out.println("Squared and cubed values ​​from 1 to " + x);
+		for (int i=1; i <= x; i++) {
+			
+			System.out.println(i + " " + ( (int) Math.pow(i, 2)) + " " + ( (int) Math.pow(i, 3)));
+		}
+
+	}
+
+}
