fd8d1782843a669f23ec630b740537b979ae9b24
João Góes <139176161+joaogoesfc@users.noreply.github.com>
2024-02-21 20:44:16 -0300

    Create ex013: great functions for strings

---
+++ exercises/ex013-interesting-functions-for-strings/src/Ex2.java
@@ -0,0 +1,18 @@
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		
+		String s = "Potato apple lemon orange";
+		
+		String[] vect = s.split(" ");
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+		System.out.println(vect[3]);
+
+
+	}
+
+}
