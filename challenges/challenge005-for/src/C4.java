cada0db7c81d44615424d99f3cacd79bf816da4e
João Góes <139176161+joaogoesfc@users.noreply.github.com>
2024-02-21 18:21:48 -0300

    Create challenge005 - C4 of loop for

---
+++ challenges/challenge005-for/src/C4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class C4 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		System.out.printf("Divisors of %d from 1 to %d\n", x, x);
+		for (int i=1; i<=x; i++) {
+			
+			if (x % i == 0) {
+				System.out.println(i);
+			}
+		
+		}
+
+	}
+
+}
