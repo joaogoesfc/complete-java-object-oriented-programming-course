5a30f6f88e3f1759e37fd5469df4c36bd648706c
João Góes <139176161+joaogoesfc@users.noreply.github.com>
2024-02-22 08:20:53 -0300

    Create ex015: problem without oriented object.

---
+++ exercises/ex015-solving-problems-without-oo/src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double xA, xB, xC, yA, yB, yC;
+		
+		System.out.println("Enter the measures of triangle X: ");
+		xA = sc.nextDouble();
+		xB = sc.nextDouble();
+		xC = sc.nextDouble();
+		
+		System.out.println("Enter the measures of triangle Y: ");
+		yA = sc.nextDouble();
+		yB = sc.nextDouble();
+		yC = sc.nextDouble();
+		
+		double areaX = calculateArea(xA, xB, xC);
+		double areaY = calculateArea(yA, yB, yC);
+		
+		System.out.printf("Triangle X area: %.4f\n", areaX);
+		System.out.printf("Triangle Y area: %.4f\n", areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("Larger area: X");
+		} else {
+			System.out.println("Larger area: Y");
+		}
+		
+		sc.close();
+	}
+	
+	public static double calculateArea(double a, double b, double c) {
+		double p = (a + b + c) / 2.0;
+		double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		return area;
+	}
+
+}
