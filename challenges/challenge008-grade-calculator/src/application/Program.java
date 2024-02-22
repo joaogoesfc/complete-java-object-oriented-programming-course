434ddd7607ee16263d9404daf65869ab98fe5f20
João Góes <139176161+joaogoesfc@users.noreply.github.com>
2024-02-22 15:06:02 -0300

    Challenge008: grade calculator

---
+++ challenges/challenge008-grade-calculator/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import intities.Student;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Student s = new Student();
+		
+		s.name = sc.nextLine();
+		s.quarter1 = sc.nextDouble();
+		s.quarter2 = sc.nextDouble();
+		s.quarter3 = sc.nextDouble();
+		
+		System.out.println("Final grade = " + s.finalGrade());
+		if (s.finalGrade() < 60) {
+			System.out.printf("Failed\nMissing %.2f points", 60 - s.finalGrade());
+		} else {System.out.println("Pass");}
+		
+		sc.close();
+	}
+
+}
---
+++ challenges/challenge008-grade-calculator/src/intities/Student.java
@@ -0,0 +1,15 @@
+package intities;
+
+public class Student {
+	
+	public String name;
+	public double quarter1;
+	public double quarter2;
+	public double quarter3;
+	
+	public double finalGrade() {
+		return quarter1 + quarter2 + quarter3;
+	}
+	
+
+}
