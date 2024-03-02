e1468c2b6dbcb1edec2dc18930b1e23db191d5ae
João Góes <goes.joaoseje@gmail.com>
2024-03-02 20:47:21 -0300

    Create: ex031_demo_stringbuilder

--- exercises/ex031_demo_stringbuilder/src/application/Program.java
+++ exercises/ex031_demo_stringbuilder/src/application/Program.java
@@ -1,10 +1,25 @@
 package application;
 
+import java.time.LocalDateTime;
+
+import entities.Comment;
+import entities.Post;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Comment c1 = new Comment("Have a nice trip!");
+		Comment c2 = new Comment("Wow that's awesome!");
+		
+		String content = "I'm going to visit this wonderful country!";
+		Post p1 = new Post(LocalDateTime.parse("2018-06-21T13:05:44"),
+				"Traveling to New Zealand",
+				content, 12);
+		p1.addComment(c1);
+		p1.addComment(c2);
+		
+		System.out.println(p1);
 	}
 
 }
