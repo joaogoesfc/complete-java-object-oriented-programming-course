d6e113ab2bd06cd1c866c59aea4d3c7b4da3ce43
João Góes <goes.joaoseje@gmail.com>
2024-03-04 15:57:46 -0300

    Create: ex032_heritage

---
+++ exercises/ex032_heritage/src/entities/Account.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Account {
+	private Integer number;
+	private String holder;
+	protected Double balance;
+	
+	
+	public Account(Integer number, String holder) {
+		this.number = number;
+		this.holder = holder;
+	}
+	
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		deposit(balance);
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(Double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(Double amount) {
+		balance -= amount;
+	}
+}
