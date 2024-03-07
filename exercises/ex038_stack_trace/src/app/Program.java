0d626a08ae4e27d4f09d56a1216154abbb820908
João Góes <goes.joaoseje@gmail.com>
2024-03-07 15:25:52 -0300

    Create: ex038_stack_trace

---
+++ exercises/ex038_stack_trace/src/app/Program.java
@@ -0,0 +1,40 @@
+package app;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		method1();
+		
+		System.out.println("End of program");
+	}
+
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+	public static void method2() {
+		System.out.println("***METHOD2 START");
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
+		} 
+		catch (InputMismatchException e) {
+			System.out.println("Invalid value, it isn't a integer number");
+		}
+		System.out.println("***METHOD2 END****");
+		sc.close();
+	}
+}
---
+++ exercises/ex038_stack_trace/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ex038_stack_trace {
+}
